868299170af71605bdd689ab27b17624d1d63f09
hepahe <henna.heinajarvi@gmail.com>
2019-05-21 21:40:28 +0300

    added some come

--- l2/script.js
+++ l2/script.js
@@ -1,1 +1,38 @@
-console.log('Hello World!!!');
+/* 
+var firstName = 'Henna';
+console.log(firstName);
+var lastName = 'Heinäjärvi';
+console.log(lastName);
+var age = 29;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job; 
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+*/
+
+// type coercion 
+var firstName = 'Henna';
+var age = 29;
+console.log(firstName + ' ' +age);
+
+var job, isMarried; 
+
+job = 'researcher';
+isMarried = false; 
+
+console.log(firstName+' is '+age+' years old '+job+'. Is she married?'+isMarried);
+
+//variable mutation
+
+age = 'twenty-nine';
+job = 'driver';
+
+alert(firstName+' is '+age+' years old '+job+'. Is she married?'+isMarried);
+
+var lastName = prompt('what is your last name?');
+console.log(firstName + ' ' +lastName);

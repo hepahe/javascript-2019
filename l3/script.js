a8857b17d61459c6e0a8ddc1361c927a7080b3d6
hepahe <henna.heinajarvi@gmail.com>
2019-05-24 17:58:46 +0300

    section 3

--- l3/script.js
+++ l3/script.js
@@ -1,20 +1,31 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// calcAge(1965);
+/*
+function calcAge (year) {
+    console.log(2019-year);
+}
 
+// retirement(1990);
 
+var retirement = function(year) {
+    console.log(65-(2019-year));
+}
 
+// variables
 
+console.log(age);
+var age = 23;
+console.log(23);
 
+function foo() {
+    var age = 65;
+    console.log(age);
+}
 
-
-
-
-
-
-
-
-
+foo();
+console.log(age);
 
 
 ///////////////////////////////////////
@@ -23,7 +34,6 @@
 
 // First scoping example
 
-/*
 var a = 'Hello!';
 first();
 
@@ -36,13 +46,11 @@ function first() {
         console.log(a + b + c);
     }
 }
-*/
-
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -58,18 +66,46 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    console.log(a + d);
 }
-*/
+
 
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
 
+calcAge(1990);
 
+function calcAge(year) {
+    console.log(2019-year);
+    console.log(this);
+}
+*/
+var john = {
+    name: 'john',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2019-this.yearOfBirth);
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction(); */
+    }
+}
+john.calculateAge(1990);
 
+var mike = {
+    name: 'mike',
+    yearOfBirth: 1984
+}
+
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
 
 
